8390b6fb09d1fd916fad66e47fe6af29c5a0c73b
museun <museun@outlook.com>
2018-11-02 14:32:07 -0400

    display is now configurable

--- src/bin/display.rs
+++ src/bin/display.rs
@@ -5,26 +5,13 @@ use std::{env, str};
 
 use shaken::modules::DisplayMessage;
 
-use termcolor::{Buffer, BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
+use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
-    let (name, mut args) = {
-        let mut args = env::args();
-        (args.next().unwrap(), args)
-    };
-    let arg = match args.next() {
-        Some(arg) => arg,
-        None => die(format!("usage: {} addr:port", name)),
-    };
-
-    let max = if let Some((w, _)) = term_size::dimensions() {
-        w - 5
-    } else {
-        60 - 5
-    } - (NAME_MAX + 2);
-
-    if let Err(err) = Client::connect(&arg, max as usize) {
+    let args = env::args().collect::<Vec<_>>();
+    let opts = Opts::parse(&args);
+    if let Err(err) = Client::connect(&opts) {
         die(format!("client error: {:?}", err))
     }
 }
@@ -34,49 +21,86 @@ fn die(s: impl AsRef<str>) -> ! {
     std::process::exit(1)
 }
 
-struct Client;
-impl Client {
-    pub fn connect(addr: &str, max: usize) -> Result<(), Error> {
-        let conn = TcpStream::connect(addr).map_err(|_e| Error::CannotConnect)?;
-        let mut reader = BufReader::new(conn).lines();
+struct Buffer<'a> {
+    writer: &'a termcolor::BufferWriter,
+    buf: termcolor::Buffer,
+    opts: &'a Opts,
+    pad: String,
+    msg: &'a DisplayMessage,
+    lines: Vec<String>,
+}
 
-        // TODO from the env
-        let use_colors = true;
+impl<'a> Buffer<'a> {
+    pub fn new(
+        buffer: &'a termcolor::BufferWriter,
+        opts: &'a Opts,
+        msg: &'a DisplayMessage,
+    ) -> Self {
+        let pad: String = std::iter::repeat(" ")
+            .take(opts.name_max + 2)
+            .collect::<String>();
 
-        let buffer = BufferWriter::stdout(if use_colors {
-            ColorChoice::Auto
-        } else {
-            ColorChoice::Never
-        });
-
-        while let Some(Ok(line)) = reader.next() {
-            let msg = serde_json::from_str::<DisplayMessage>(&line).expect("valid json");
-            let mut buf = buffer.buffer();
-            let mut buf = Self::add_name(&msg, &mut buf);
-            let lines = Self::split_lines(&msg, max);
-            let buf = Self::print_lines(&lines, &mut buf, max);
-            buffer.print(&buf).expect("print");
+        Self {
+            writer: buffer,
+            buf: buffer.buffer(),
+            pad,
+            opts,
+            msg,
+            lines: vec![], // this allocates 7..
         }
-        Ok(())
     }
 
-    fn add_name<'a>(msg: &DisplayMessage, buf: &'a mut Buffer) -> &'a mut Buffer {
+    pub fn print(mut self) {
+        self.add_name();
+        self.split_lines();
+        self.write_lines();
+        self.writer.print(&self.buf).expect("print");
+    }
+
+    fn add_name(&mut self) {
         let mut spec = ColorSpec::new();
-        let (r, g, b) = (msg.color.0, msg.color.1, msg.color.2);
+        let (r, g, b) = (self.msg.color.0, self.msg.color.1, self.msg.color.2);
         spec.set_fg(Some(Color::Rgb(r, g, b)));
-        buf.set_color(&spec).expect("set color");
+        self.buf.set_color(&spec).expect("set color");
 
-        let name = truncate(msg.name.clone());
-        write!(buf, "{:>offset$}: ", name, offset = NAME_MAX + 1);
-        buf.reset().expect("reset");
-        buf
+        let mut name = self.msg.name.clone();
+        let name = self.truncate(&mut name);
+        write!(
+            self.buf,
+            "{:>offset$}: ",
+            name,
+            offset = self.opts.name_max + 1
+        );
+        self.buf.reset().expect("reset");
     }
 
-    fn split_lines(msg: &DisplayMessage, max: usize) -> Vec<String> {
+    fn write_lines(&mut self) {
+        for (i, s) in self.lines.iter().map(|s| s.trim_left()).enumerate() {
+            if i == 0 {
+                write!(self.buf, "{}", s);
+            } else {
+                write!(self.buf, "{}{}{}", self.opts.left, self.pad, s);
+            }
+            if self.lines.len() == 1 {
+                writeln!(self.buf);
+                continue;
+            }
+            if i < self.lines.len() - 1 {
+                let len = self.opts.line_max.saturating_sub(s.len());
+                writeln!(self.buf, "{: >width$}", self.opts.right, width = len);
+            } else {
+                writeln!(self.buf);
+            }
+        }
+    }
+
+    fn split_lines(&mut self) {
+        let max = self.opts.line_max;
+
         let mut lines = vec![];
         let mut line = String::new();
-        for s in msg.data.split_word_bounds() {
-            if s.len() > max {
+        for s in self.msg.data.split_word_bounds() {
+            if s.len() >= max {
                 let mut tmp = String::new();
                 for c in s.chars() {
                     if line.len() == max {
@@ -99,7 +123,7 @@ impl Client {
                 }
                 continue;
             }
-            if line.len() + s.len() > max {
+            if line.len() + s.len() >= max {
                 lines.push(line.clone());
                 line.clear();
             }
@@ -108,50 +132,123 @@ impl Client {
         if !line.is_empty() {
             lines.push(line);
         }
-        lines
+
+        std::mem::replace(&mut self.lines, lines);
     }
 
-    fn print_lines<'a>(lines: &[String], buf: &'a mut Buffer, max: usize) -> &'a mut Buffer {
-        let pad: String = std::iter::repeat(" ")
-            .take(NAME_MAX + 2)
-            .collect::<String>(); // probably should be passed in as an arg
-
-        for (i, s) in lines.iter().map(|s| s.trim_left()).enumerate() {
-            if i == 0 {
-                write!(buf, "{}", s);
-            } else {
-                write!(buf, "{}{}{}", LEFT_FRINGE, pad, s);
-            }
-            if lines.len() == 1 {
-                writeln!(buf);
-                continue;
-            }
-            if i < lines.len() - 1 {
-                let len = max.saturating_sub(s.len()) + 3;
-                writeln!(buf, "{: >width$}", RIGHT_FRINGE, width = len);
-            } else {
-                writeln!(buf);
-            }
+    fn truncate<'b>(&self, name: &'b mut String) -> &'b String {
+        let max = self.opts.name_max;
+        if name.len() <= max {
+            return name;
         }
-        buf
+        name.truncate(max);
+        name.insert(max, '…');
+        name
     }
 }
 
-const NAME_MAX: usize = 10;
-const LEFT_FRINGE: char = '\u{1F4A9}';
-const RIGHT_FRINGE: char = '\u{1F6BD}';
+struct Client;
+impl Client {
+    pub fn connect(opts: &Opts) -> Result<(), Error> {
+        let conn = TcpStream::connect(&opts.addr).map_err(|_e| Error::CannotConnect)?;
+        let mut reader = BufReader::new(conn).lines();
 
-fn truncate(mut s: String) -> String {
-    if s.len() <= NAME_MAX {
-        return s;
+        let colors = if env::var("NO_COLOR").is_err() {
+            ColorChoice::Auto
+        } else {
+            ColorChoice::Never
+        };
+        let buffer = BufferWriter::stdout(colors);
+
+        while let Some(Ok(line)) = reader.next() {
+            let msg = serde_json::from_str::<DisplayMessage>(&line).expect("valid json");
+            Buffer::new(&buffer, &opts, &msg).print();
+        }
+        Ok(())
     }
-
-    s.truncate(NAME_MAX);
-    s.insert(NAME_MAX, '…');
-    s
 }
 
 #[derive(Debug, PartialEq)]
 enum Error {
     CannotConnect,
+}
+
+struct Opts {
+    left: char,
+    right: char,
+    addr: String,
+    line_max: usize,
+    name_max: usize,
+}
+
+// this is ugly
+impl Opts {
+    pub fn parse(args: &[String]) -> Self {
+        use getopts::Options;
+        let mut opts = Options::new();
+        opts.optflag("h", "help", "prints help information");
+        opts.optopt(
+            "a",
+            "addr",
+            "the address to connect to. defaults to 'localhost:51001`",
+            "ip:port",
+        );
+        opts.optopt(
+            "l",
+            "left",
+            "left fringe character. defaults to '\u{2937}'",
+            "char",
+        );
+        opts.optopt(
+            "r",
+            "right",
+            "right fringe character. defaults to '\u{2936}'",
+            "char",
+        );
+        opts.optopt(
+            "n",
+            "name",
+            "width of names before truncation. defaults to '10'",
+            "size",
+        );
+
+        // TODO or default terminal size
+
+        let matches = match opts.parse(&args[1..]) {
+            Ok(opts) => opts,
+            Err(err) => die(format!("couldn't parse args: {}", err)),
+        };
+
+        if matches.opt_present("h") {
+            print!("{}", opts.usage(&format!("usage: {}", &args[0])));
+            std::process::exit(0);
+        }
+
+        let addr = matches
+            .opt_get_default("a", "localhost:51001".into())
+            .expect("address arg");
+
+        let name_max = matches.opt_get_default("n", 10usize).expect("name_max arg");
+
+        let left = matches
+            .opt_get_default("l", '\u{2937}')
+            .expect("left fringe arg");
+
+        let right = matches
+            .opt_get_default("r", '\u{2936}')
+            .expect("right fringe arg");
+
+        let line_max = term_size::dimensions()
+            .and_then(|(w, _)| Some(w - 2))
+            .unwrap_or_else(|| 60 - 2)
+            - (name_max); // fringe+space
+
+        Opts {
+            left,
+            right,
+            addr,
+            line_max,
+            name_max,
+        }
+    }
 }
