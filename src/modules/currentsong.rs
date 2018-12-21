2779174b4750e11f8cb0d15c55df381fc4181319
museun <museun@outlook.com>
2018-12-21 16:14:20 -0500

    timestamp + previous song

--- src/modules/currentsong.rs
+++ src/modules/currentsong.rs
@@ -2,7 +2,9 @@ use crate::prelude::*;
 use std::collections::VecDeque;
 use std::net::TcpListener;
 use std::sync::{Arc, RwLock};
+use std::time::Duration;
 
+use chrono::prelude::*;
 use log::*;
 use serde_derive::{Deserialize, Serialize};
 
@@ -29,7 +31,8 @@ impl CurrentSong {
             map: CommandMap::create(
                 "CurrentSong",
                 &[
-                    ("!song", Self::info_command), //
+                    ("!song", Self::info_command),     //
+                    ("!prevsong", Self::prev_command), //
                 ],
             )?,
             // TODO get this from the config
@@ -42,6 +45,8 @@ impl CurrentSong {
     }
 
     fn info_command(&mut self, _: &Request) -> Option<Response> {
+        let now = Utc::now(); // get this as early as possible
+
         if let Some(info) = self.listener.current() {
             let id = self
                 .pattern
@@ -50,16 +55,50 @@ impl CurrentSong {
                 .as_str();
 
             if let Some(vid) = lookup_info(id) {
-                return multi!(
-                    say!("{}", vid.title), //
-                    say!("{}", info.url)   //
-                );
+                let start = Utc.timestamp(info.ts as i64, 0);
+                let dur = chrono::Duration::from_std(vid.duration).unwrap();
+                let time = dur - (now - start);
+                let delta = (dur - time).num_seconds();
+
+                return if delta > 0 {
+                    say!("\"{}\" {}&t={}s", vid.title, info.url, delta)
+                } else {
+                    info!("not currently playing");
+                    // TODO maybe note that this isn't actually playing
+                    say!("\"{}\" {}", vid.title, info.url)
+                };
             } else {
                 return say!("{}", info.url);
             }
         }
 
         reply!("no song is currently playing")
+    }
+
+    fn prev_command(&mut self, _: &Request) -> Option<Response> {
+        let list = self.listener.list();
+        if let Some(info) = list.get(list.len().saturating_sub(2)) {
+            let id = self
+                .pattern
+                .captures(&info.url)
+                .and_then(|s| s.name("id"))?
+                .as_str();
+
+            let start = Utc.timestamp(info.ts as i64, 0);
+
+            return if let Some(vid) = lookup_info(id) {
+                say!(
+                    "previous song: (started at {}) \"{}\" {}",
+                    start,
+                    vid.title,
+                    info.url
+                )
+            } else {
+                say!("previous song: (started at {}) {}", start, info.url)
+            };
+        }
+
+        reply!("I don't remember a song playing then")
     }
 }
 
@@ -102,7 +141,7 @@ impl Listener {
     }
 
     pub fn current(&self) -> Option<Info> {
-        self.list.read().unwrap().back().cloned() // to not block
+        self.list.read().unwrap().back().cloned() // to not hold onto the mutex
     }
 
     pub fn list(&self) -> Vec<Info> {
@@ -117,6 +156,7 @@ impl Listener {
 struct YoutubeVideo {
     id: String,
     title: String,
+    duration: Duration,
 }
 
 fn lookup_info(id: &str) -> Option<YoutubeVideo> {
@@ -128,8 +168,11 @@ fn lookup_info(id: &str) -> Option<YoutubeVideo> {
 
     let map = &[
         ("id", id),
-        ("part", "snippet"),
-        ("fields", "items(id, snippet(title))"),
+        ("part", "snippet,contentDetails"),
+        (
+            "fields",
+            "items(id, snippet(title), contentDetails(duration))",
+        ),
         ("key", api_key.as_str()),
     ];
 
@@ -165,14 +208,50 @@ fn lookup_info(id: &str) -> Option<YoutubeVideo> {
         .ok()?;
 
     let val = val.get("items")?.as_array()?.get(0)?;
+
     let id = val.get("id")?.as_str()?;
     let title = val
         .get("snippet")
         .and_then(|val| val.get("title"))?
         .as_str()?;
 
+    let duration = val
+        .get("contentDetails")
+        .and_then(|val| val.get("duration"))?
+        .as_str()
+        .map(from_iso8601)?;
+
     Some(YoutubeVideo {
         id: id.to_string(),
         title: title.to_string(),
+        duration,
     })
 }
+
+fn from_iso8601(period: &str) -> Duration {
+    let list = period.split(|c: char| !c.is_numeric());
+
+    let (mut total, mut index) = (0, 0);
+    for el in list.rev().filter(|el| !el.is_empty()) {
+        total += el.parse::<u64>().unwrap() * u64::pow(60, index);
+        index += 1;
+    }
+
+    Duration::from_secs(total)
+}
+
+// The length of the video. The property value is an ISO 8601 duration. For
+// example, for a video that is at least one minute long and less than one hour
+// long, the duration is in the format PT#M#S, in which the letters PT indicate
+// that the value specifies a period of time, and the letters M and S refer to
+// length in minutes and seconds, respectively. The # characters preceding the M
+// and S letters are both integers that specify the number of minutes (or
+// seconds) of the video. For example, a value of PT15M33S indicates that the
+// video is 15 minutes and 33 seconds long.
+
+// If the video is at least one hour long, the duration is in the format
+// PT#H#M#S, in which the # preceding the letter H specifies the length of the
+// video in hours and all of the other details are the same as described above.
+// If the video is at least one day long, the letters P and T are separated, and
+// the value's format is P#DT#H#M#S. Please refer to the ISO 8601 specification
+// for complete details.
